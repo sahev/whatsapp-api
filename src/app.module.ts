ba8df763f059eac4714979a8e4d015cb27478bf9
sahev <s4mus.sh@gmail.com>
2022-09-06 21:28:11 -0300

    clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,23 +4,11 @@ import { SessionWebSocketModule } from './infra/modules/session.ws.module';
 import { WhatsAppModule } from './infra/modules/whatsapp.module';
 import { ConfigModule } from '@nestjs/config';
 import { ConfigurationModule } from './infra/modules/configuration.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ 
       envFilePath: `${process.cwd()}/src/infra/configuration/${process.env.NODE_ENV}.env`
-    }), 
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './src/infra/database/database.test.db',
-      logging: true,
-      synchronize: true,
-      entities: [],
-      migrationsTableName: 'migrations_table',
-      migrations: [
-        "./src/infra/database/migrations/**.ts"
-      ]
     }),
     SessionWebSocketModule,  
     SessionModule, 
