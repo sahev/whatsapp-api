9bfe65f399cca928d5d86144a66833ce49ec57f7
sahev <s4mus.sh@gmail.com>
2023-07-05 20:55:57 -0300

    fix

--- updaterepository.sh
+++ updaterepository.sh
@@ -1,9 +1,9 @@
 git pull origin main
 
-docker stop whatsapp-api
-docker rm whatsapp-api
-docker rmi whatsapp-api
+sudo docker stop whatsapp-api
+sudo docker rm whatsapp-api
+sudo docker rmi whatsapp-api
 
-docker build -t whatsapp-api .
+sudo docker build -t whatsapp-api .
 
-docker run -d -p 4040:4040 --name whatsapp-api --restart=always whatsapp-api
+sudo docker run -d -p 4040:4040 --name whatsapp-api --restart=always whatsapp-api
