435068535adea865cb609f6d3ddba16ecdd9b255
sahev <s4mus.sh@gmail.com>
2023-07-05 21:51:51 -0300

    fix port

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ var ip = require("ip");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.API_PORT);
+  await app.listen(process.env.API_PORT ?? 3000);
 }
 
 bootstrap();
 
-console.log(ip.address(), process.env.API_PORT);
+console.log(ip.address(), process.env.API_PORT ?? 3000);
